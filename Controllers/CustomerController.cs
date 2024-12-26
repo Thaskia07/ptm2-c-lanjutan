87c729560a6db7479ffc39cb69f8c64ca3869f89
thaskia <thaskia67@gmail.com>
2024-12-26 11:42:51 +0700

    ptm 3c#lanjutan

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using pertemuan_2.Models.DB;
 using pertemuan_2.Services;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -16,10 +17,9 @@ namespace pertemuan_2.Controllers
         }
 
 
-        // GET: api/<CustomerController>
+        //// GET: api/<CustomerController>
+        ///berdasarkan list
         [HttpGet]
-
-
         public IActionResult Get()
         {
             var customerList = _customerServices.GetListCustomers();
@@ -27,29 +27,84 @@ namespace pertemuan_2.Controllers
 
         }
 
-        // GET api/<CustomerController>/5
+
+
+
         [HttpGet("{id}")]
-        public string Get(int id)
+
+        //berdasarkan id
+        public IActionResult GetCustomer(int id)
         {
-            return "value";
+            var customer = _customerServices.GetCustomerById(id);
+            if (customer == null)
+            {
+                return NotFound("Customer tidak ditemukan.");
+            }
+            return Ok(customer);
         }
 
-        // POST api/<CustomerController>
+
+
+
+
         [HttpPost]
-        public void Post([FromBody] string value)
+
+        //menggunakan iactionresult karena lebih fleksibel karena ada return ok dan return badrequest
+        public IActionResult Post(Customer customer)
         {
+            var insertCustomer = _customerServices.CreateCustomer(customer);
+            if(insertCustomer)
+            {
+                //return StatusCode(StatusCodes) untuk statuscode api
+                return Ok("insert customer succes");
+            }
+            return BadRequest("insert customer failed");
         }
+
 
         // PUT api/<CustomerController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        [HttpPut]
+        public IActionResult Put(Customer customer)
         {
+            try
+            {
+
+                var updateCustomer = _customerServices.UpdateCustomer(customer);
+                if (updateCustomer)
+                {
+                    return Ok("update customert succes!");
+                }
+                return BadRequest("update customer failed");
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message.ToString());
+                throw;
+            }
         }
 
-        // DELETE api/<CustomerController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+
+        public IActionResult DeleteCustomer(int id)
         {
+            try
+            {
+                var deleteCustomer = _customerServices.DeleteCustomer(id);
+                if(deleteCustomer)
+                {
+                    return Ok("delete customer succes");
+                }
+                return BadRequest("delete failede");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message.ToString());
+
+                throw;
+            }
         }
+
+
     }
 }
--- Services/CustomerServices.cs
+++ Services/CustomerServices.cs
@@ -14,12 +14,90 @@ namespace pertemuan_2.Services
         }
 
         //tabel customer dari kelas customer
+
         public List<Customer> GetListCustomers()
         {
+            //mencari customer dengan list
             //di datas isinya list
             //getlistcustomer dipanggil di controller
             var datas = _context.Customers.ToList();
             return datas;
+        }
+
+        public Customer GetCustomerById(int id)
+        {
+            // Mencari customer berdasarkan ID
+            var customer = _context.Customers.FirstOrDefault(c => c.Id == id);
+            return customer;
+        }
+
+
+        public bool CreateCustomer(Customer customer)
+        {
+            try
+            {
+                _context.Customers.Add(customer);
+                _context.SaveChanges();
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+
+        //model customer
+        public bool UpdateCustomer(Customer customer)
+        {
+            try
+            {
+                var customerOld = _context.Customers.Where(x => x.Id == customer.Id).FirstOrDefault();
+                //harus melihat dlu kondisinya dan butuh validasi
+                if (customerOld != null)
+                {
+                    customerOld.Name = customer.Name;
+                    customerOld.Address = customer.Address;
+                    customerOld.City = customer.City;
+                    customerOld.PhoneNumber = customer.PhoneNumber;
+
+                    _context.SaveChanges();
+
+                    return true;
+
+                }
+
+                return false;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+
+        public bool DeleteCustomer(int id) {
+            try
+            {
+                var costomerData = _context.Customers.
+                FirstOrDefault(x => x.Id == id);
+
+                if (costomerData != null)
+                {
+                    _context.Customers.Remove(costomerData);
+                    _context.SaveChanges();
+
+                    return true;
+
+                }
+                return false;
+            }
+
+            catch (Exception)
+            {
+                throw;
+            }
         }
     }
 }
