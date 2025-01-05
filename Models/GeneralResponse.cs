c01ddf92f61e9d8a65d477a83c91296846c9f352
thaskia <thaskia67@gmail.com>
2025-01-05 11:45:04 +0700

    mplementGeneralResponse 1

---
+++ Models/GeneralResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace pertemuan_2.Models
+{
+    public class GeneralResponse
+    {
+        
+        public string StatusCode { get; set; }
+        public string StatusDesc { get; set; }
+        public object Data { get; set; }    
+
+    }
+}
